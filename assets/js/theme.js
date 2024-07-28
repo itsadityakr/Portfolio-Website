65cf7469f80754ed2cdfde81866c72ef6a9a86ad
Aditya Kumar <itsadityakr27@gmail.com>
2024-07-28 21:46:05 +0530

    ok

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,6 +1,5 @@
 // Selectors
 const themeToggleBtn = document.querySelector(".theme-toggle");
-const themeIcon = document.querySelector("#theme-icon");
 
 // Get the current theme from localStorage
 const theme = localStorage.getItem("theme");
@@ -8,18 +7,14 @@ const theme = localStorage.getItem("theme");
 // Apply the saved theme
 if (theme) {
     document.body.classList.add(theme);
-    // Set the appropriate image based on the saved theme
-    themeIcon.src = theme === "dark-mode" ? "../assets/icons/light.png" : "../assets/icons/dark.png";
 }
 
 const handleThemeToggle = () => {
     document.body.classList.toggle("dark-mode");
     if (document.body.classList.contains("dark-mode")) {
         localStorage.setItem("theme", "dark-mode");
-        themeIcon.src = "../assets/icons/light.png"; // Set to dark mode image
     } else {
         localStorage.removeItem("theme");
-        themeIcon.src = "../assets/icons/dark.png"; // Set to light mode image
     }
 };
 
